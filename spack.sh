13318a89ada4a16a4bbd85a96c0f54513d1ed88f
Derek Ryan Strong <dstrong@usc.edu>
2024-01-04 13:51:58 -0800

    Update spack.sh

--- spack.sh
+++ spack.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
 # source spack.sh
 module purge
+export SPACK_PYTHON=/spack/2206/apps/linux-centos7-x86_64_v3/gcc-11.3.0/python-3.11.3-gl2q3yz/bin/python
 source /project/ttrojan_123/spack/share/spack/setup-env.sh
