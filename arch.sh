d798c09f0c92a329a27102a7a5b3b4c571ac8610
Derek Ryan Strong <dstrong@usc.edu>
2024-07-25 14:51:47 -0700

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+# source arch.sh
 
 # Detect microarchitecture and activate corresponding environment
 
@@ -7,9 +7,9 @@ source /project/ttrojan_123/spack.sh
 arch=$(spack arch -t)
 
 if [[ "$arch" = "skylake_avx512" ]]; then
-  spack env activate skylake_avx512
+  spack env activate myenv-skylake-avx512
 elif [[ "$arch" = "zen3" ]]; then
-  spack env activate zen3
+  spack env activate myenv-zen3
 else
   printf "%s%s\n" "Environment not available for microarchitecture: " "$arch"
   exit 1
